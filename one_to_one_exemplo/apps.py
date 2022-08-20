d462466c21c9d5c45024df4bd9ba88fe1aacdfa9
academiapython <academiapython.py@gmail.com>
2022-08-20 12:12:36 -0300

    Implementação de signal personalizado.

--- one_to_one_exemplo/apps.py
+++ one_to_one_exemplo/apps.py
@@ -1,5 +1,7 @@
 from django.apps import AppConfig
+from django.dispatch import Signal
 
 class OneToOneExemploConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'one_to_one_exemplo'
+    menor_idade_signal = Signal()
--- one_to_one_exemplo/models.py
+++ one_to_one_exemplo/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 from django.db.models.signals import post_save
+from . apps import OneToOneExemploConfig
 
 from django.dispatch import receiver
 from abstract_user_exemplo.models import Pessoa
-
 
 class Perfil(models.Model):
     user = models.OneToOneField(Pessoa, on_delete=models.CASCADE)
@@ -13,3 +13,11 @@ class Perfil(models.Model):
 @receiver(post_save, sender=Pessoa, dispatch_uid="id_post_save_pessoa")
 def minha_funcao_callback(sender, **kwargs):
     print("Um novo registro foi inserido na tabela Pessoa")
+
+    instance = kwargs.get('instance')
+    if instance.idade < 18:
+        OneToOneExemploConfig.menor_idade_signal.send(sender=Pessoa)
+
+@receiver(OneToOneExemploConfig.menor_idade_signal, sender=Pessoa)
+def cadastrou_menor_idade(sender, **kwargs):
+    print('Interceptando ou recebendo informação de cadastro de menores de idade.')
